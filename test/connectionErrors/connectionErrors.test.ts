ef41b63c0eee009d6d1a1ae4c02fdff3fa489362
euberdeveloper <euberdeveloper@gmail.com>
2020-01-10 15:36:20 +0100

    test: connection error

---
+++ test/connectionErrors/connectionErrors.test.ts
@@ -0,0 +1,26 @@
+import { MongoScanner, ConnectionError } from '../../source/index';
+
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+import { expect } from 'chai';
+
+export default function () {
+
+    describe('Test: connection errors', function () {
+
+        it(`Should eventually throw a connection error`, async function () {
+
+            const scanner = new MongoScanner('mongodb://mona:1234');
+
+            const willFail = async function() {
+                await scanner.startConnection();
+            }
+
+            return expect(willFail()).to.be.rejected;
+
+        });
+        
+    });
+
+}
--- test/test.ts
+++ test/test.ts
@@ -3,13 +3,17 @@ import listCollections from './listCollections/listCollections.test';
 import getSchema from './getSchema/getSchema.test';
 import persistentConnection from './persistentConnection/persistentConnection.test';
 import clearCache from './clearCache/clearCache.test';
+import connectionErrors from './connectionErrors/connectionErrors.test';
 
 describe('MongoBack module tests', function() {
+
+    this.timeout(0);
 
     listDatabases();
     listCollections();
     getSchema();
     persistentConnection();
     clearCache();
+    connectionErrors();
 
 });
