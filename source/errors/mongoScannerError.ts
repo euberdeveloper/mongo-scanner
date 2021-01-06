bef8af08a7781cfba4c806886826992308cedd1e
euberdeveloper <euberdeveloper@gmail.com>
2021-01-07 00:18:08 +0100

    fix(mongoscannererror.ts): fix instanceof after transpiling
    On typescript, instanceof does not work for classes extending Error if an ugly trick is not
    implemented

--- source/errors/mongoScannerError.ts
+++ source/errors/mongoScannerError.ts
@@ -1,8 +1,17 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
 /**
  * An error occured with the mongo-scanner module.
  */
 export class MongoScannerError extends Error {
+    public __proto__: Error;
+
     constructor(message?: string) {
+        // This includes a trick in order to make the instanceof properly work
+        const trueProto = new.target.prototype;
         super(message);
+        this.__proto__ = trueProto;
+
+        this.name = 'MongoScannerError';
     }
 }
