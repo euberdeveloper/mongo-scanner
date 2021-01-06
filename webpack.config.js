c12307f95b08e5d648b0e00a5ef4f3ea75e8d958
euberdeveloper <euberdeveloper@gmail.com>
2021-01-07 00:31:55 +0100

    build(webpack): add bundler

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const DtsBundleWebpack = require('dts-bundle-webpack');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+    target: 'node',
+    mode: 'production',
+    // devtool: 'source-map',
+    entry: {
+        index: './source/index.ts',
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        plugins: [new TsconfigPathsPlugin({
+            configFile: './source/tsconfig.json',
+            extensions: ['.ts', '.js']
+        })]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts/,
+                include: path.resolve(__dirname, 'source'),
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: { compiler: 'ttypescript' }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new DtsBundleWebpack({
+            name: 'mongo-scanner',
+            main: 'dist/index.d.ts',
+            out: '../bundled/index.d.ts'
+        })
+    ],
+    externals: [nodeExternals()],
+    output: {
+        path: path.resolve(__dirname, 'bundled'),
+        filename: 'index.js',
+        library: 'mongo-scanner',
+        libraryTarget: 'umd',
+        globalObject: 'this',
+        umdNamedDefine: true
+    }
+};
