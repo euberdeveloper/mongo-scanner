4b127d6574967a7e8b45da8a67ac36021de9da3f
euberdeveloper <euberdeveloper@gmail.com>
2021-01-06 22:13:42 +0100

    style: add eslint and prettier config files

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: path.join(__dirname, 'tsconfig.json')
+    },
+    plugins: ['prettier'],
+    extends: [
+        '@euberdeveloper/typescript',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended'
+    ],
+    rules: {
+        '@typescript-eslint/no-unnecessary-condition': 'off'
+    }
+};
